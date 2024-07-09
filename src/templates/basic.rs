18357d5cdca142b249467134a9ca1daf786ad6ba
Jade Ellis <jade@ellis.link>
2024-07-09 18:00:38 +0100

    refactor: Add i18n-ally extension configuration

--- src/templates/basic.rs
+++ src/templates/basic.rs
@@ -1,4 +1,4 @@
-use mf1::t_l_string as t;
+use mf1::t_l_string as tl;
 use mrml::{mjml, text::Text};
 use mrmx_macros::view;
 use serde_json::Value;
@@ -11,7 +11,7 @@ pub(crate) fn basic(_: Value, l: Locale) -> Result<mjml::Mjml, TemplateError> {
     Ok(view! {
         <mjml>
           <mj-body>
-            <mj-button>"Hello world in "{Text::from(t!(l, lang)).into()}!</mj-button>
+            <mj-button>"Hello world in "{Text::from(tl!(l, lang)).into()}!</mj-button>
           </mj-body>
         </mjml>
     })
