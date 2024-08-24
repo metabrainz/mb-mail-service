7099fa610848339e3394d50021d65441a7632d64
Jade Ellis <jade@ellis.link>
2024-08-24 17:53:25 +0100

    feat: Subscription template uses translations

--- src/templates/subscription.rs
+++ src/templates/subscription.rs
@@ -1,3 +1,5 @@
+use std::borrow::Borrow;
+
 use mf1::t_l_string as tl;
 use mrml::{mjml::Mjml, text::Text};
 use mrmx::WithAttribute;
@@ -23,6 +25,7 @@ pub(crate) fn subscription(params: Value, l: Locale) -> Result<Mjml, TemplateErr
         <mjml>
         <mj-head>
             { head().into() }
+            <mj-title>{ tl!(l, subscription.title ).borrow() }</mj-title>
         </mj-head>
         <mj-body width="500px" padding="0">
             <mj-section padding="20px 0">
@@ -31,12 +34,12 @@ pub(crate) fn subscription(params: Value, l: Locale) -> Result<Mjml, TemplateErr
 
                 <mj-text>
                     <p>{ Text::from(tl!(l, greeting_line, name = to_name )).into() }</p>
-                    <p>"New edits have been added for artists that you've subscribed to."</p>
+                    <p>{ Text::from(tl!(l, subscription.info )).into() }</p>
                 </mj-text>
 
                 <mj-wrapper mj-class="wrapper" >
                     <mj-text>
-                        <h2 >"Changes for your subscribed artists:"</h2>
+                        <h2 >{ Text::from(tl!(l, subscription.subscribed_artists_changes )).into() }</h2>
                         <ul>
                             <li><a href="https://musicbrainz.org/artist/8d8d8a80-f74f-4f21-a44c-518cd6944ed2/edits">"Nathan (English EDM artist)"</a>" (0 open, 1 applied)"</li>
                         </ul>
@@ -44,27 +47,27 @@ pub(crate) fn subscription(params: Value, l: Locale) -> Result<Mjml, TemplateErr
                 </mj-wrapper>
                 <mj-wrapper mj-class="wrapper" >
                     <mj-text>
-                        <h2>"All open edits:"</h2>
+                        <h2>{ Text::from(tl!(l, subscription.open_edits )).into() }</h2>
                         <ul>
-                            <li><a href="https://musicbrainz.org/edit/subscribed?open=1">All open edits for your subscribed entities</a></li>
-                            <li><a href="https://musicbrainz.org/edit/subscribed_editors?open=1">All open edits by your subscribed editors</a></li>
+                            <li><a href="https://musicbrainz.org/edit/subscribed?open=1">{ Text::from(tl!(l, subscription.open_edits_subscribed_entities )).into() }</a></li>
+                            <li><a href="https://musicbrainz.org/edit/subscribed_editors?open=1">{ Text::from(tl!(l, subscription.open_edits_subscribed_editors )).into() }</a></li>
                         </ul>
 
 
                     </mj-text>
                 </mj-wrapper>
                 <mj-text>
-                    <p>Thanks for subscribing and voting!</p>
-                    <p><em>"—  The MetaBrainz community"</em></p>
+                    <p>{ Text::from(tl!(l, subscription.thanks)).into() }</p>
+                    <p><em>{ Text::from(tl!(l, metabrainz_signoff)).into() }</em></p>
                 </mj-text>
                 <mj-divider padding="10px 15px" border-color="#F5F5F5" border-width="3px" />
                 <mj-text font-size="12px" color="#8D8D8D">
-                    <p>
-                        "This is a notification that edits have been added for artists, labels, collections and editors to whom you subscribed on the MusicBrainz web site. "
-                        <a href="https://musicbrainz.org/user/Jellis16/subscriptions">Click here to view or edit your subscription list</a>"."
-                    </p>
-                    <p>"Do not reply to this message. If you need help, please "<a href="https://metabrainz.org/contact">contact us</a>.</p>
 
+                    <p>{ Text::from(tl!(l, subscription.about)).into() }</p>
+                    <p>
+                        <a href="https://musicbrainz.org/user/Jellis16/subscriptions">{ Text::from(tl!(l, change_subscription_settings)).into() }</a>
+                    </p>
+                    <p>{ Text::from(tl!(l, do_not_reply)).into() }</p>
                 </mj-text>
 
             </mj-column>
