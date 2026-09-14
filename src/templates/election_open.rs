3d55c11296989c3f34a2e69c089a064864322346
Jade Ellis <jade@ellis.link>
2026-09-14 18:02:34 +0100

    feat: Changes to election templates
    - Use the wrapper / background to highlight the action in
      the open/start emails
    - Add missing signoff and footer

--- src/templates/election_open.rs
+++ src/templates/election_open.rs
@@ -85,11 +85,21 @@ pub(crate) fn election_open(params: Value, l: Locale) -> Result<Mjml, TemplateEr
                     </p>
                 </mj-text>
 
+                <mj-wrapper mj-class="wrapper">
+                    <mj-text>
+                        <p><strong>{ Text::from(tl!(l, election_start.participate )).into() }</strong></p>
+                        <p>
+                            <a href={election_url}>{ Text::from(encode_text(election_url)).into()}</a>
+                        </p>
+                    </mj-text>
+                </mj-wrapper>
+
                 <mj-text>
-                    <p><strong>{ Text::from(tl!(l, election_open.participate )).into() }</strong></p>
-                    <p>
-                        <a href={election_url}>{ Text::from(encode_text(election_url)).into()}</a>
-                    </p>
+                    <p><em>{ Text::from(tl!(l, metabrainz_signoff)).into() }</em></p>
+                </mj-text>
+                <mj-divider padding="10px 15px" border-color="#F5F5F5" border-width="3px" />
+                <mj-text font-size="12px" color="#8D8D8D">
+                    <p>{ Text::from(tl!(l, do_not_reply)).into() }</p>
                 </mj-text>
             </mj-column>
         </mj-section>
